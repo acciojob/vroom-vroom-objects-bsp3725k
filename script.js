5699a49c88e93cfb496962be06bd5d0bea62df57
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-26 12:25:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 function Car(make, model) {
 		this.make=make;
 		this.model=model;
-	}
-	Car.prototype.getMakeModel=function() {
+}
+Car.prototype.getMakeModel=function() {
 		return `${this.make} ${this.model}`;
-	};
+};
 
 function SportsCar(make, model, topSpeed) {
         Car.call(this,make,model);
 		this.topSpeed = topSpeed;
 	}
-	SportsCar.protytype=Object.create(Car.prototype);
-    SportsCar.protytype.constructor=SportsCar;
-   SportsCar.prototype.getTopSpeed=function() {
+SportsCar.protytype=Object.create(Car.prototype);
+SportsCar.protytype.constructor=SportsCar;
+SportsCar.prototype.getTopSpeed=function() {
 	return this.topSpeed;
-}
+};
 
 // Do not change the code below
 window.Car = Car;
