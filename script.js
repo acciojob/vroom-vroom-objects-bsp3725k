d8386e7ce18429d88d44f0dd3ec4c3435ef3190f
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-26 12:46:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 // Complete the js code
 function Car(make, model) {
-		this.make=make;
-		this.model=model;
+    this.make = make;
+    this.model = model;
 }
-Car.prototype.getMakeModel=function() {
-		return `${this.make} ${this.model}`;
+
+// Add a method to the Car prototype to get the make and model
+Car.prototype.getMakeModel = function () {
+    return `${this.make} ${this.model}`;
 };
 
 function SportsCar(make, model, topSpeed) {
-        Car.call(this,make,model);
-		this.topSpeed = topSpeed;
-	}
-SportsCar.protytype=Object.create(Car.prototype);
-SportsCar.protytype.constructor=SportsCar;
-SportsCar.prototype.getTopSpeed=function() {
-	return this.topSpeed;
-};
+    // Call the Car constructor to initialize make and model
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed; // Initialize topSpeed
+}
 
+// Inherit the Car prototype in SportsCar prototype
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Reset the constructor of SportsCar
+SportsCar.prototype.constructor = SportsCar;
+
+// Add a method to the SportsCar prototype to get the top speed
+SportsCar.prototype.getTopSpeed = function () {
+    return this.topSpeed;
+};
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
